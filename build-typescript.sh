a80fcf63450556e4ce7013c1fc6f323b5780e67e
Milan Hauth <milahu@gmail.com>
2022-11-27 12:59:12 +0100

    add relative size to description

--- build-typescript.sh
+++ build-typescript.sh
@@ -1,6 +1,18 @@
 pkg=$(mktemp -d)
 lib=$pkg/lib
 
+divide_1000() {
+  # fixed-point math divison by 1000
+  if [[ "$1" == "" ]]; then echo 0; return; fi
+  a=000$1; a=${a:0: -3}
+  while [ "${a:0:1}" = 0 ]; do a=${a:1}; done
+  if [ -z "$a" ]; then a=0; fi
+  b=000$1; b=${b: -3}
+  echo $a.$b
+}
+
+size1=$(du -sb node_modules/typescript/ | cut -d$'\t' -f1)
+
 cp -r node_modules/typescript/* $pkg
 cp README.md $pkg
 
@@ -15,9 +27,16 @@ pushd $pkg
 npm pkg set \
 	name=@kidonng/typescript \
 	version="${1:-0.0.0}" \
-	description="A smaller redistribution of TypeScript" \
+	description="A smaller redistribution of TypeScript. x.xxx% of the original size" \
 	repository=kidonng/typescript
 npm pkg delete bin scripts.prepare homepage bugs
+
+size2=$(du -sb . | cut -d$'\t' -f1)
+relative_size=$(divide_1000 $(( size2 * 100 * 1000 / size1)))
+echo "relative_size = $size2 / $size1 = ${relative_size}%"
+
+npm pkg set \
+	description="A smaller redistribution of TypeScript. ${relative_size}% of the original size" \
 
 if [[ -n "$1" ]]; then
 	npm publish --access public
